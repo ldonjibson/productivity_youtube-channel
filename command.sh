b1087d6812106ff235d2c88463de993d206e5bbd
ldonjibson <ajibolaolayanju@mail.com>
2026-06-17 11:23:12 +0100

    local

--- command.sh
+++ command.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+# curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 
-curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
-  sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+#   sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+#   sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-sudo apt-get update -qq
-sudo apt-get install -y -qq nvidia-container-toolkit
+# sudo apt-get update -qq
+# sudo apt-get install -y -qq nvidia-container-toolkit
 
-sudo nvidia-ctk runtime configure --runtime=docker
-sudo systemctl restart docker
+# sudo nvidia-ctk runtime configure --runtime=docker
+# sudo systemctl restart docker
 
 docker run -d --gpus all \
   -p 8000:8000 \
